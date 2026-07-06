bc1c484e367c5e1f12c254a432b049981e6e7fe0
dinethchatumina2011 <dinethchatumina847@gmail.com>
2026-07-06 05:52:39 -0700

    Update autobio.js

--- plugins/autobio.js
+++ plugins/autobio.js
@@ -36,7 +36,7 @@ cmd({
 ┃❃╰───────────────
 ╰═════════════════⊷
 
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𓆩𝐙𝐀𝐈𝐃𝐈-𝐌𝐃𓆪`;
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʜᴀᴛʜᴜᴡᴀ-xᴍᴅ`;
 
       await conn.sendMessage(from, {
         text: display,
@@ -64,7 +64,7 @@ cmd({
 ┃❃╰───────────────
 ╰═════════════════⊷
 
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𓆩𝐙𝐀𝐈𝐃𝐈-𝐌𝐃𓆪`;
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʜᴀᴛʜᴜᴡᴀ-xᴍᴅ`;
 
     await conn.sendMessage(from, {
       text: display,
@@ -90,7 +90,7 @@ async function updateBio(conn) {
 
   try {
     const uptime = clockString(process.uptime() * 1000);
-    const botname = config.BOT_NAME || "𓆩𝐙𝐀𝐈𝐃𝐈-𝐌𝐃𓆪";
+    const botname = config.BOT_NAME || "𝐂𝐇𝐀𝐓𝐇𝐔𝐖𝐀-𝐗𝐌𝐃";
 
     const bio = `👑 ${botname} ACTIVE (${uptime}) 👑`;
     await conn.updateProfileStatus(bio);
